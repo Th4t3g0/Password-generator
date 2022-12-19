1eb3c121e1e8840cf71303261b833bd12a44000f
Th4t3g0 <113315881+Th4t3g0@users.noreply.github.com>
2022-12-19 23:21:52 +0200

    update

--- a.js
+++ a.js
@@ -1,5 +1,5 @@
 let i=document.getElementById("i");
-//i.disabled=true;
+i.disabled=true;
 const passwordChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '.', ',', ';', ':', '/', '?', '<', '>', '[', ']', '{', '}', '|', '~', '`'];
 var upassoword;
 function passwordGenerator(list) {
@@ -37,8 +37,10 @@ i.setAttribute("type","text")
 
 c=document.getElementById("c");
 c.addEventListener("click",function(){
+i.disabled=false;
 i.select();
 document.execCommand("copy");
+i.disabled=true;
 c.textContent = 'Done!';
 setTimeout(() => {
     c.textContent = 'Ctrl+C';
