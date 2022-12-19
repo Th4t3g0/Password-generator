a919543eb36559b274075a6aa2aea2763d895697
Th4t3g0 <113315881+Th4t3g0@users.noreply.github.com>
2022-12-19 22:19:30 +0200

    initial commit

---
+++ a.js
@@ -0,0 +1,22 @@
+document.getElementById("i").disabled=true;
+const passwordChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '.', ',', ';', ':', '/', '?', '<', '>', '[', ']', '{', '}', '|', '~', '`'];
+
+function passwordGenerator(list) {
+let password="";
+const n = Math.floor(Math.random()*(6)+6);
+const a=shuffle(passwordChars).slice(0,n);
+for (let name of a) {
+    password+=name;
+}
+return password
+}
+
+function shuffle(array) {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+//shuffle function written by chatGPT
